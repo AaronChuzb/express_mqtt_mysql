3773a85126574f13c1c182886d36b923fd1641dd
Aaron Chu <571388081@qq.com>
2021-05-08 18:35:33 +0800

    *init

--- index.js
+++ index.js
@@ -3,21 +3,10 @@ const db = require("./db.js");
 const app = express()
 const port = 4000
 
-/* //连接数据库
-var db = mysql.createConnection({ 
-  host: "rm-7xv3h4rryx3sa9uo37o.mysql.rds.aliyuncs.com",
-  port: "3306",
-  user: "root",
-  password: "19030125cC",
-  database: "data"
-}); */
-
-
-
 app.get('/', (req, res) => {
-  db.query("SELECT * FROM datas",[], (results, fields) => {
+  db.query("SELECT * FROM datas",[], async (results, fields) => {
    console.log(results)
-   res.send(results)
+   await res.send(results)
   })
 })
 
