22cfc7e94d7dd0b6f7e7b36021811e29cc23ce92
AaronChuzb <45120061+AaronChuzb@users.noreply.github.com>
2021-05-11 20:16:35 +0800

    *add 添加傻瓜式控制台输出

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  db.query("SELECT * FROM datas",[], async (results, fields) => {
+  res.send('服务端正在运行')
+  /* db.query("SELECT * FROM datas",[], async (results, fields) => {
    console.log(results)
    await res.send(results)
-  })
+  }) */
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log("服务端开始运行，请不要关闭此窗口。")
+  console.log(`后台运行在：http://localhost:${port}`)
+  console.log("可在浏览器打开此网址查看")
 })
